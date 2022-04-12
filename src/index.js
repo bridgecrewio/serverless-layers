ab69541c9189dedaf58cf91fbb854ab574174eb8
yuval yacoby <yacoby133@gmail.com>
2022-04-12 17:32:46 +0300

    check for custom hash

--- src/index.js
+++ src/index.js
@@ -226,34 +226,6 @@ class ServerlessLayers {
     });
   }
 
-  async hasCustomHashChanged() {
-    if (!this.settings.customHash) {
-      return false;
-    }
-
-    const hashFileName = 'customHash.json';
-    const remoteHashFile = await this.bucketService.getFile(hashFileName);
-
-    if (!remoteHashFile) {
-      this.log('no previous custom hash found, putting new remote hash');
-      await this.bucketService.putFile(
-        hashFileName, JSON.stringify({ hash: this.settings.customHash })
-      );
-      return true;
-    }
-
-    const { hash: remoteHash } = JSON.parse(remoteHashFile);
-    if (remoteHash === this.settings.customHash) {
-      return false;
-    }
-
-    await this.bucketService.putFile(
-      hashFileName, JSON.stringify({ hash: this.settings.customHash })
-    );
-    this.log('identified custom hash change!');
-    return true;
-  }
-
   async main() {
     const {
       arn,
